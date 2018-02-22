d7bb5b7b9b8bfc5d034179a1d51b28218124cb1c
newgermes <kukhar.vitalii@gmail.com>
2018-02-22 12:45:27 +0200

    add example.com

--- js/main.js
+++ js/main.js
@@ -2,9 +2,9 @@
 
 (function() {
     var memoryBanner = new adRock({
-            urls: ['https://braindislab.com/', 
-                    'https://braindislab.com/stati/', 
-                    'https://braindislab.com/metody-lecheniya/*.*',
+            urls: ['https://example.com/', 
+                    'https://example.com/stati/', 
+                    'https://example.com/metody-lecheniya/*.*',
                     ], // ['href01, href02']
             // urls: 'http://localhost:8080/',
             insertAfter: '.art_hd_tetxt:1', // #id, .class:(0,1,2,3,4)           
